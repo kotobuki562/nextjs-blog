99f5ae925090123bde64603ff0b062f6a93de656
MaQueen <shusineko@icloud.com>
2020-12-15 16:58:38 +0900

    ブログ記事を生成

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
+          私の名前はぼ寿司です。渋谷のスタートアップ企業でフロントエンドエンジニアとして働いています。React.jsを得意とするエンジニアになります。直近のプロジェクトでFlutterを使ったアプリ開発も予定しております。お見知り置きを。
+        </p>
+        <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
