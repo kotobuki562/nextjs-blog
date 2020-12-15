272d0da06fefab0921266611da3213a504426683
MaQueen <shusineko@icloud.com>
2020-12-15 17:20:14 +0900

    ブログの記事を公開

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,38 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          私の名前はぼ寿司です。渋谷のスタートアップ企業でフロントエンドエンジニアとして働いています。React.jsを得意とするエンジニアになります。直近のプロジェクトでFlutterを使ったアプリ開発も予定しております。お見知り置きを。
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-        <Link href="/posts/first-post">
-          <a>POSt</a>
-        </Link>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title, text }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+              <br />
+              {text}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
